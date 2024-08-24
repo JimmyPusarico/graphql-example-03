db2ce6f7905c3048b1dab3b491fca292258c6b92
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-08-24 11:44:26 -0400

    borramos código del array en memoria y añadimos el servicio de cantidad de libros

--- src/index.ts
+++ src/index.ts
@@ -75,29 +75,6 @@ const typeDefs = `#graphql
   }
 `;
 
-const books = [
-  {
-    id: 'd26fd654-f4d4-4b98-91e5-6d8c9569aed6',
-    title: 'The Awakening',
-    description: 'The Awakening es una novela de la escritora estadounidense Kate Chopin.',
-    publisher: 'W W Norton & Co Inc',
-    genre: 'NONE',
-    publishYear: 1899,
-    authorName: 'Kate Chopin'
-  },
-  {
-    id: '35b19ead-3aa9-415e-a46d-6621e1604119',
-    title: 'City of Glass',
-    description: 'Ciudad de cristal es el tercer libro de la saga Cazadores de Sombras, escrita por Cassandra Clare. Fue publicada originalmente en Estados Unidos.',
-    isbn: '978-0140097313',
-    publisher: 'Simon & Schuster',
-    genre: 'FANTASY',
-    publishYear: 2009,
-    authorName: 'Paul Auster',
-    authorNationality: 'Estadounidense'
-  },
-];
-
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
@@ -106,7 +83,10 @@ const resolvers = {
       const { data: books } = await axios.get(`${process.env.API_URL}/books/`);
       return books;
     },
-    getBooksCount: () => books.length,
+    getBooksCount: async () => {
+      const { data: books } = await axios.get(`${process.env.API_URL}/books/`);
+      return books.length;
+    },
     getBook: async (root, args) => {
       const { id } = args;
       const { data: book } = await axios.get(`${process.env.API_URL}/books/${id}`);
@@ -139,32 +119,11 @@ const resolvers = {
     },
     
     updateBook: (root, args) => {
-      const updatedBookIndex = books.findIndex(book => book.id === args.id);
-
-      if (updatedBookIndex === -1) return null;
-
-      const book = books[updatedBookIndex];
-      const updatedBook = {...book,
-        title: args.title ? args.title : book.title,
-        description: args.description ? args.description : book.description,
-        isbn: args.isbn ? args.isbn : book.isbn,
-        publisher: args.publisher ? args.publisher : book.publisher,
-        genre: args.genre ? args.genre : book.genre,
-        publishYear: args.publishYear ? args.publishYear : book.publishYear,
-        authorName: args.authorName ? args.authorName : book.authorName,
-        authorNationality: args.authorNationality ? args.authorNationality : book.authorNationality
-      };
-      books[updatedBookIndex] = updatedBook;
-      return updatedBook;
+      
     },
 
     deleteBook: (root, { id }) => {
-      const deletedBookIndex = books.findIndex(book => book.id === id);
-
-      if (deletedBookIndex === -1) return null;
-
-      const deletedBook = books.splice(deletedBookIndex, 1)[0];
-      return deletedBook;
+      
     }
   }
 };
